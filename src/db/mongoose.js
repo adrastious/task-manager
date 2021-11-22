7250aa2f1e4fe3f04d8e2323274cf72d27ed5135
Carlos Del Angel <carlos@mitunetwork.com>
2021-11-21 23:13:07 -0600

    latest

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb://${process.env.MONGODB_URL}/task-manager-api`, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
